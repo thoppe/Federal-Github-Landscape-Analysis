8b16b9dfada8578334020be27fb0d22a33ec7ac0
thoppe <travis.hoppe@gmail.com>
2025-01-10 14:34:57 -0500

    Fix table

--- src/A2_size_stats.py
+++ src/A2_size_stats.py
@@ -2,11 +2,13 @@ import pandas as pd
 
 f_repos = "data/repos_by_cumulative_popularity.csv"
 df = pd.read_csv(f_repos)
+df = df[~df["fork"]]
 
+##########################################################################
 
+df[df["stargazers_count"] >= 1]
 df = df.sort_values("size", ascending=False)
 
-print(df.columns)
 cols = ["full_name", "description", "size", "stargazers_count", "language"]
 df = df[cols]
 
@@ -17,9 +19,7 @@ df = df.rename(columns=lambda x: x.title())
 df = df.set_index("Repository")
 
 print(df[:20].to_string())
-
 exit()
-
 
 ##########################################################################
 
@@ -36,3 +36,5 @@ df = df.rename(columns=lambda x: x.title())
 df = df.set_index("Repository")
 
 print(df[:20].to_string())
+exit()
+
